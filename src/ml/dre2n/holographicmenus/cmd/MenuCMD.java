db072a2830b5b50785012959071c12a3075b550a
Daniel Saukel <sa.da@unitybox.de>
2015-08-03 02:47:38 +0200

    Added /menu [open|close] [player name] [menu]

--- src/ml/dre2n/holographicmenus/cmd/MenuCMD.java
+++ src/ml/dre2n/holographicmenus/cmd/MenuCMD.java
@@ -1,51 +1,102 @@
 package ml.dre2n.holographicmenus.cmd;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import ml.dre2n.holographicmenus.HolographicMenus;
 import ml.dre2n.holographicmenus.storage.ConfigStorage;
 import ml.dre2n.holographicmenus.task.InitializeHoloTask;
 import ml.dre2n.holographicmenus.util.VariableUtil;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
+import com.gmail.filoghost.holographicdisplays.api.Hologram;
+import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
+
 public class MenuCMD implements CommandExecutor {
 	
 	Plugin plugin = HolographicMenus.plugin;
+	Server server = Bukkit.getServer();
+	
+	Player player;
+	String type;
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String commandlabel, String[] args) {
 		// Let's get a player entity from the command sender.
-		Player player = (Player) sender;
+		player = (Player) sender;
 		
 		if (commandlabel.equalsIgnoreCase("menu") || commandlabel.equalsIgnoreCase("m")) {
 			
-			// Open a custom menu, if the sender enters a menu name and has permission to do so
-			if (args.length >= 1 && sender.hasPermission("holographicmenus.menu." + args[0])) {
+			// Open a menu for another player
+			if (args[0].equalsIgnoreCase("open") && args.length >= 3 && sender.hasPermission("holographicmenus.openclose")) {
+				player = server.getPlayer(args[1]);
+				type = args[2];
 				
-				// Check if menu exists
-				if (ConfigStorage.getData().getString("menus." + args[0] + ".pages") != null) {
-					Bukkit.getScheduler().runTask(plugin, new InitializeHoloTask(player, args[0]));
-					
-				// Menu doesn't exist
-				} else {
-					VariableUtil.sendMessage("error.noMenu", player);
-				}
+				openMenu();
+				
+			// Close a menu for another player
+			} else if (args[0].equalsIgnoreCase("close") && args.length == 2 && sender.hasPermission("holographicmenus.openclose")) {
+				player = server.getPlayer(args[1]);
+				
+				closeMenu();
+				
+			// Open a custom menu, if the sender enters a menu name and has permission to do so
+			} else if (args.length >= 1 && sender.hasPermission("holographicmenus.menu." + args[0])) {
+				type = args[0];
+				
+				openMenu();
 				
 			// If no arguments are entered, just open the main menu
 			} else if (args.length == 0 && sender.hasPermission("holographicmenus.menu.main")) {
-				Bukkit.getScheduler().runTask(plugin, new InitializeHoloTask(player, "main"));
+				type = "main";
+				
+				openMenu();
 				
 			// Permission check failed
 			} else {
 				VariableUtil.sendMessage("error.noPermission", player);
 			}
+			
 			return true;
 		}
 		return false;
+	}
+	
+	void openMenu() {
+		
+		// Check if menu exists
+		if (ConfigStorage.getData().getString("menus." + type + ".pages") != null) {
+			Bukkit.getScheduler().runTask(plugin, new InitializeHoloTask(player, type));
+			
+		// Menu doesn't exist
+		} else {
+			VariableUtil.sendMessage("error.noMenu", player);
+		}
+		
+	}
+	
+	void closeMenu() {
+		// All holograms created by this plugin
+		Collection<Hologram> allHolograms = HologramsAPI.getHolograms(plugin);
+		
+		// Check all of them...
+	    for (Iterator<Hologram> iterator = allHolograms.iterator(); iterator.hasNext();) {
+	        Hologram hologram = (Hologram) iterator.next();
+	        
+	        if (hologram.getVisibilityManager().isVisibleTo(player)) {
+	        	// ...and delete the hologram if it is visible only to the player.
+	        	hologram.delete();
+	        }
+	        
+	    }
+	    
 	}
 	
 }
